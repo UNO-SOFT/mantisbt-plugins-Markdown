199daacd1396eeed24ecffa35964b15bce925fce
Tamás Gulácsi <tgulacsi78@gmail.com>
2016-04-15 14:12:15 +0200

    MantisCoreFormatting may be turned off

--- Markdown.php
+++ Markdown.php
@@ -38,35 +38,71 @@ class MarkdownPlugin extends MantisFormattingPlugin {
 		);
 	}
 
-	function display_formatted( $p_event, $p_string, $p_extra=null ) {
-		static $s_core_formats = null;
-		if( null === $s_core_formats ) {
-			$s_core_formats = config_get( 'plugin_MantisCoreFormatting_process_text' ) == TRUE;
+	function display_formatted( $p_event, $p_string, $p_multiline = true ) {
+		static $s_core_formats, $s_core_installed;
+		static $s_urls, $s_buglinks;
+		if( null === $s_core_installed ) {
+			$s_core_installed = plugin_is_installed( 'MantisCoreFormatting' );
+			if( !$s_core_installed ) {
+				$s_core_formats = FALSE;
+			} else {
+				$s_core_formats = config_get( 'plugin_MantisCoreFormatting_process_text' ) == TRUE;
+			}
 			$this->pd->setBreaksEnabled( !$s_core_cormats );
 		}
-		echo '<!--' . var_export( $s_core_formats, true ) . '-->';
 		if( !$s_core_formats ) {
-			if( substr( $p_string, 0, 4 ) === '#MD#' ) {
-				$p_string = substr( $p_string, 4 );
+			$t_string = $p_string;
+			if( substr( $t_string, 0, 4 ) === '#MD#' ) {
+				// Markdown formatting
+				$t_string = md_strip_mark( $t_string );
+				$t_string = md_process_bugnote_link( $t_string );
+				$t_string = $this->pd->text( $t_string );
+				return strip_p( $t_string );
 			}
-			$res = $this->pd->text( $p_string );
-			$res = strip_p( $res );
-			return $res;
+			// Mantis formatting
+			// replicate the functionality of MantisCoreFormatting->text
+			$t_string = string_strip_hrefs( $t_string );
+			$t_string = string_html_specialchars( $t_string );
+			$t_string = string_restore_valid_html_tags( $t_string, $p_multiline );
+
+			if( $p_multiline ) {
+				$t_string = string_preserve_spaces_at_bol( $t_string );
+				$t_string = string_nl2br( $t_string );
+			}
+
+			if( $s_core_installed ) {
+				return $t_string;
+			}
+
+			if( null === $s_urls ) {
+				$s_urls = config_get( 'plugin_MantisCoreFormatting_process_urls' ) == FALSE;
+				$s_buglinks = config_get( 'plugin_MantisCoreFormatting_process_buglinks' ) == FALSE;
+			}
+			if( $s_urls ) {
+				$t_string = string_insert_hrefs( $t_string );
+			}
+			if( $s_buglinks ) {
+				$t_string = string_process_bug_link( $t_string );
+				$t_string = string_process_bugnote_link( $t_string );
+			}
+			return $t_string;
 		}
+
+		// Already Mantis-formatted
 		if( substr( $p_string, 0, 4 ) !== '#MD#' ) {
 			return $p_string;
 		}
-		$p_string = substr( $p_string, 4 );
-		$p_string = str_replace( "<br />\r\n", "\n", $p_string );
-		$olen = strlen( $p_string );
-		$p_string = str_replace( ' * ', ' \* ', $p_string );
-		$changed_stars = strlen( $p_string ) - $olen;
-		$res = $this->pd->text( $p_string );
-		$res = strip_p( $res );
+		$t_string = md_strip_mark( $p_string );
+		$t_string = str_replace( "<br />\r\n", "\n", $t_string );
+		$olen = strlen( $t_string );
+		$t_string = str_replace( ' * ', ' \* ', $t_string );
+		$changed_stars = strlen( $t_string ) - $olen;
+		$t_string = $this->pd->text( $t_string );
+		$t_string = strip_p( $t_string );
 		if( $changed_stars > 0 ) {
-			$res = str_replace( ' \* ', ' * ', $res );
+			$t_string = str_replace( ' \* ', ' * ', $t_string );
 		}
-		return $res;
+		return $t_string;
 	}
 
 }
@@ -76,4 +112,32 @@ function strip_p( $p_string ) {
 		return substr( $p_string, 3, strlen( $p_string ) - 3 - 4 );
 	}
 	return $p_string;
+}
+
+function md_strip_mark( $p_string ) {
+	if( substr( $p_string, 0, 4 ) !== '#MD#' ) {
+		return $p_string;
+	}
+	$p_string = substr( $p_string, 4 );
+	if( substr( $p_string, 0, 1 ) === "\n" ) {
+		return substr( $p_string, 1 );
+	}
+	return $p_string;
+}
+
+function md_process_bugnote_link( $p_string ) {
+	static $s_tag = null;
+	if( $s_tag === null ) {
+		$s_tag = config_get( 'bugnote_link_tag' );
+	}
+	if( $s_tag == '' ) {
+		return $p_string;
+	}
+	return preg_replace_callback(
+		'/(^|[^\w])(' . preg_quote( $s_tag, '/' ) . '\d+)\b/',
+		function( $p_array ) {
+			return $p_array[1] . '[' . $p_array[2] . '](' . string_process_bugnote_link( $p_array[2], false ) . ')';
+		},
+		$p_string
+	);
 }
